7f4294d7bf43cfe119a8f5a221bb511383174b78
小野隆規 <petite@PetiteEtoile.local>
2020-10-05 13:14:51 +0900

    first

---
+++ packing/find_one.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+const int h = 10;
+const int w = 6;
+const int grid_size = h*w;
+
+bitset<grid_size> grid;
+
+vector<bitset<grid_size>> mino = {
+    ("110" + "0"*w + "011" + "0"*w + "010"), //F
+    ("")
+};
+
+int main(){
+    return 0;
+}
